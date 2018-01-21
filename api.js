6648477d6d82c1f454ae7f3e2c6743c60d189154
michaeljqzq <michaeljqzq@gmail.com>
2018-01-21 12:07:00 +0000

    Update Static API

--- api.js
+++ api.js
@@ -298,7 +298,7 @@ module.exports = require("util");
 
 
 module.exports = {
-    api: 'http://e2e-portal-api.azurewebsites.net',
+    api: 'https://e2e-portal-api.azurewebsites.net',
     startTime: '2018-01-19T07:17:00Z'
 };
 
